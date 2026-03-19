838b72339f42cb92e743f6303ba2235429d59d93
KristijanZic <kristijan.zic@10x.engineering>
2026-03-19 22:40:33 +0100

    [3.3] functions: added function with param and return value

--- 3.3/functions/src/main.rs
+++ 3.3/functions/src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     print_labeled_measurement(5, 'h');
     println!("The value is: {}", five());
+    println!("The value 5+1 is: {}", plus_one(5));
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
@@ -9,4 +10,8 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
 
 fn five() -> i32 {
     5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
