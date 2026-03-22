aed201ca2d75e1c1c6b12f45cc1285df0c50971e
KristijanZic <kristijan.zic@10x.engineering>
2026-03-22 01:03:10 +0100

    [5.2] rectangles: introduced debugging print

--- 5.2/rectangles/src/main.rs
+++ 5.2/rectangles/src/main.rs
@@ -5,8 +5,16 @@ fn main() {
     };
 
     println!("The area of the rectangle is {} square pixels", area(&rect));
+
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!("rect1 is {rect1:#?}");
 }
 
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
