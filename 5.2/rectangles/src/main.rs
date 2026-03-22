e371163dae9f9a9d794815f5ab30688f92b81892
KristijanZic <kristijan.zic@10x.engineering>
2026-03-22 01:09:55 +0100

    [5.2] rectangles: introduced debugging via dbg! macro

--- 5.2/rectangles/src/main.rs
+++ 5.2/rectangles/src/main.rs
@@ -12,6 +12,14 @@ fn main() {
     };
 
     println!("rect1 is {rect1:#?}");
+
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect2);
 }
 
 #[derive(Debug)]
